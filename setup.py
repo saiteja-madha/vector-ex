02eeb0ac657e4b7fae27bad2ea73f603a30fd826
saiteja-madha <git@saitejamadha.dev>
2025-01-06 19:50:54 -0700

    setup.py dependencies & packages

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ setup(
     install_requires=[
         "langchain-chroma>=0.1.4",
         "langchain-community>=0.3.14",
-        "langchain-core>=0.3.29" "langchain-huggingface>=0.1.2",
+        "langchain-core>=0.3.29",
+        "langchain-huggingface>=0.1.2",
         "langchain-text-splitters>=0.3.4",
         "PyMuPDF>=1.25.0",
         "sentence-transformers>=3.3.0",
